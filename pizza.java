c747a9c0897c327342218ed3206f9eb6df3bd32f
Jorgealmaraz0204 <141947952+Jorgealmaraz0204@users.noreply.github.com>
2026-07-12 12:40:16 -0600

    Add files via upload

---
+++ pizza.java
@@ -0,0 +1,25 @@
+
+public class pizza
+{
+    // instance variables - replace the example below with your own
+    private int nombre,tipo_de_masa,ingrediente;
+    
+    
+    public pizza()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
